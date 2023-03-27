514e5f5a88309cace6811540cab422ddb32ecf7d
gem-charuGarg <charu.garg@geminisolutions.com>
2023-03-27 15:51:11 +0530

    ATS API automation

---
+++ src/main/java/com/qa/ats/stepdefinition/Steps.java
@@ -0,0 +1,4 @@
+package com.qa.ats.stepdefinition;
+public class Steps {
+
+}
---
+++ src/main/java/com/qa/ats/utils/GlobalVariable.java
@@ -0,0 +1,5 @@
+package com.qa.ats.utils;
+
+public class GlobalVariable {
+    public static final String BASE_URL = "https://devapi.geminisolutions.com/";
+}
