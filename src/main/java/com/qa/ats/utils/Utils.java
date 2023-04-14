ae21c4fce35ea4ff3b506663db6752f54ccc082d
Tripta Sahni <Tripta.sahni@Geminisolutions.com>
2023-04-14 12:17:36 +0530

    myUpdatedCommitNegativeScenarios

--- src/main/java/com/qa/ats/utils/Utils.java
+++ src/main/java/com/qa/ats/utils/Utils.java
@@ -8,6 +8,7 @@ import com.gemini.generic.utils.ProjectConfigData;
 
 import com.google.gson.*;
 import com.qa.ats.stepdefinition.ApplicantStep;
+//import com.qa.ats.stepdefinition.AtsRegression;
 import com.qa.ats.stepdefinition.InterviewStep;
 import com.qa.ats.stepdefinition.AtsHealthCheck;
 import org.slf4j.Logger;
@@ -203,8 +204,8 @@ public class Utils {
                 GemTestReporter.addTestStep("Response Body", "No-Response", STATUS.INFO);
             }
 
-        } 
-        if (method.equals("post")) {
+        }
+        if (method.equals("post")&&response.getStatus()>=200&&response.getStatus()<=201) {
             return (response.getStatus() + "," + JsonParser.parseString(response.getResponseBody()).getAsJsonObject().get("object"));
         }
 
@@ -258,7 +259,7 @@ public class Utils {
             GemTestReporter.addTestStep(method.toUpperCase() + " Request Verification ", method.toUpperCase() + " Request Did not Executed Successfully", STATUS.FAIL);
             GemTestReporter.addTestStep("Response Message", response.getResponseMessage(), STATUS.INFO);
         }
-        if (method.equals("post"))
+        if  (method.equals("post")&&response.getStatus()>=200&&response.getStatus()<=201)
             return (response.getStatus() + "," + JsonParser.parseString(response.getResponseBody()).getAsJsonObject().get("object").getAsJsonObject().get("feedbackId").toString());
         else
             return String.valueOf(response.getStatus());
@@ -318,9 +319,10 @@ public class Utils {
             GemTestReporter.addTestStep("POST Request Verification", "POST request executed Successfully", STATUS.PASS);
             js = (JsonObject) JsonParser.parseString(EntityUtils.toString(httpresponse.getEntity()));
             GemTestReporter.addTestStep("Response Body", String.valueOf(js), STATUS.INFO);
+            if(String.valueOf(js).contains("400")==false)
             GemTestReporter.addTestStep("Response Message", js.get("message").getAsString(), STATUS.INFO);
             arr[0] = httpresponse.getStatusLine().getStatusCode();
-            if (method.equals("post")) {
+            if (method.equals("post")&&arr[0]>=200 && arr[0]<=201) {
                 arr[1] = Integer.parseInt(String.valueOf(js.get("object").getAsJsonObject().get("jobId")));
                 return (arr[0] + "," + arr[1]);
             } else
@@ -329,7 +331,7 @@ public class Utils {
             logger.info("Request doesn't Executed Successfully ", exception);
             GemTestReporter.addTestStep(method.toUpperCase() + " Request Verification ", method.toUpperCase() + " Request Did not Executed Successfully", STATUS.FAIL);
         }
-        if (method.equals("post")) {
+        if (method.equals("post")&&arr[0]>=200 && arr[0]<=201) {
             return (arr[0] + "," + arr[1]);
         } else
             return String.valueOf(arr[0]);
@@ -379,6 +381,7 @@ public class Utils {
         else
             return String.valueOf(arr[0]);
     }
+
 }
 
 
