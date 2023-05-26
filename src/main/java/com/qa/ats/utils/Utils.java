b4632cea72757d78369a5807d692c463e8d4fb31
gem-charuGarg <charu.garg@geminisolutions.com>
2023-05-26 17:00:17 +0530

    Update Changes

--- src/main/java/com/qa/ats/utils/Utils.java
+++ src/main/java/com/qa/ats/utils/Utils.java
@@ -608,53 +608,19 @@ public class Utils {
             String u = "";
             u = GlobalVariable.BASE_URL + ProjectConfigData.getProperty(url);
             GemTestReporter.addTestStep("Url of the test case", u, STATUS.INFO);
-            CloseableHttpClient httpclient = HttpClients.createDefault();
-            MultipartEntityBuilder entitybuilder = entityBuilderFileParserForMultipleApplicantsForWrongHeader(keys, values, method, url,name);
-            HttpEntity mutiPartHttpEntity = entitybuilder.build();
-            RequestBuilder reqbuilder = null;
-            if (method.equalsIgnoreCase("post")) {
-                reqbuilder = RequestBuilder.post(u);
-            } else if (method.equalsIgnoreCase("put")) {
-                reqbuilder = RequestBuilder.put(u);
-            }
-            else if (method.equalsIgnoreCase("delete"))
-                reqbuilder=RequestBuilder.delete(u);
-            reqbuilder.setEntity(mutiPartHttpEntity);
-            HttpUriRequest multipartRequest = reqbuilder.build();
-            multipartRequest.setHeader(new BasicHeader("X-REMOTE-USER-EMAIL", "nipun.jain@geminisolutions.com"));
-            HttpResponse httpresponse = httpclient.execute(multipartRequest);
-            arr[0] = httpresponse.getStatusLine().getStatusCode();
-            GemTestReporter.addTestStep("POST Request Verification", "POST request executed Successfully", STATUS.PASS);
-            JsonObject js = (JsonObject) JsonParser.parseString(EntityUtils.toString(httpresponse.getEntity()));
-            GemTestReporter.addTestStep("Response Body", String.valueOf(js), STATUS.INFO);
-            if(arr[0]==200 || arr[0]==201)
-                GemTestReporter.addTestStep("Response Message", js.get("message").getAsString(), STATUS.INFO);
-            return String.valueOf(arr[0]);
-        } catch (Exception exception) {
-            logger.info("Request doesn't Executed Successfully ", exception);
-            GemTestReporter.addTestStep(method.toUpperCase() + " Request Verification ", method.toUpperCase() + " Request Did not Executed Successfully", STATUS.FAIL);
-        }
-        return String.valueOf(arr[0]);
-    }
-    public static String addMultipleApplicants(String url, String method, Map<String, String> headers, List<String> keys, List<String> values) {
-        int arr[] = new int[2];
-        try {
-            String u = "";
-            u = GlobalVariable.BASE_URL + ProjectConfigData.getProperty(url);
-            GemTestReporter.addTestStep("Url of the test case", u, STATUS.INFO);
             CloseableHttpClient httpClient = HttpClients.createDefault();
             MultipartEntityBuilder entityBuilder = entityBuilderFileParserForMultipleApplicantsForWrongHeader(keys, values, method, url,name);
             HttpEntity multiPartHttpEntity = entityBuilder.build();
-            RequestBuilder reqbuilder = null;
+            RequestBuilder reqBuilder = null;
             if (method.equalsIgnoreCase("post")) {
-                reqbuilder = RequestBuilder.post(u);
+                reqBuilder = RequestBuilder.post(u);
             } else if (method.equalsIgnoreCase("put")) {
-                reqbuilder = RequestBuilder.put(u);
+                reqBuilder = RequestBuilder.put(u);
             }
             else if (method.equalsIgnoreCase("delete"))
-                reqbuilder=RequestBuilder.delete(u);
-            reqbuilder.setEntity(multiPartHttpEntity);
-            HttpUriRequest multipartRequest = reqbuilder.build();
+                reqBuilder=RequestBuilder.delete(u);
+            reqBuilder.setEntity(multiPartHttpEntity);
+            HttpUriRequest multipartRequest = reqBuilder.build();
             multipartRequest.setHeader(new BasicHeader("X-REMOTE-USER-EMAIL", "nipun.jain@geminisolutions.com"));
             HttpResponse httpresponse = httpClient.execute(multipartRequest);
             arr[0] = httpresponse.getStatusLine().getStatusCode();
@@ -670,6 +636,7 @@ public class Utils {
         }
         return String.valueOf(arr[0]);
     }
+
     public static String addMultipleApplicants(String url, String method, Map<String, String> headers, List<String> keys, List<String> values) {
         int[] arr = new int[2];
         try {
@@ -690,7 +657,7 @@ public class Utils {
             reqBuilder.setEntity(multiPartHttpEntity);
             HttpUriRequest multipartRequest = reqBuilder.build();
             multipartRequest.setHeader(new BasicHeader("X-REMOTE-USER-EMAIL", "nipun.jain@geminisolutions.com"));
-            HttpResponse httpResponse = httpclient.execute(multipartRequest);
+            HttpResponse httpResponse = httpClient.execute(multipartRequest);
             arr[0] = httpResponse.getStatusLine().getStatusCode();
             GemTestReporter.addTestStep("POST Request Verification", "POST request executed Successfully", STATUS.PASS);
             JsonObject js = (JsonObject) JsonParser.parseString(EntityUtils.toString(httpResponse.getEntity()));
